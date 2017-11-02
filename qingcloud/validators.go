cf121c83c91dc5fa08b50d29d0a31144175d75be
runzexia <root@junwuhui.cn>
2017-11-02 16:27:38 +0800

    move validator_func to validators.go , change the return value of validator_func to  increase readability , add validators_test

--- qingcloud/validators.go
+++ qingcloud/validators.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net"
 	"strings"
+
+	"github.com/hashicorp/terraform/helper/schema"
 )
 
 func validateRouterVxnetsCIDR(v interface{}, k string) (ws []string, errors []error) {
@@ -34,4 +36,48 @@ func validateNetworkCIDR(v interface{}, k string) (ws []string, errors []error) 
 		return
 	}
 	return
+}
+
+func withinArrayString(limits ...string) schema.SchemaValidateFunc {
+	var limitsMap = make(map[string]bool)
+	for _, v := range limits {
+		limitsMap[v] = true
+	}
+
+	return func(v interface{}, k string) (ws []string, errors []error) {
+		value := v.(string)
+		if limitsMap[value] {
+			return
+		}
+		errors = append(errors, fmt.Errorf("%q (%q) doesn't match", k, value))
+		return
+	}
+}
+
+func withinArrayInt(limits ...int) schema.SchemaValidateFunc {
+	var limitsMap = make(map[int]bool)
+	for _, v := range limits {
+		limitsMap[v] = true
+	}
+
+	return func(v interface{}, k string) (ws []string, errors []error) {
+		value := v.(int)
+		if limitsMap[value] {
+			return
+		}
+		errors = append(errors, fmt.Errorf("%q (%q) doesn't match", k, value))
+		return
+	}
+}
+
+func withinArrayIntRange(begin, end int) schema.SchemaValidateFunc {
+
+	return func(v interface{}, k string) (ws []string, errors []error) {
+		value := v.(int)
+		if value >= begin && value <= end {
+			return
+		}
+		errors = append(errors, fmt.Errorf("%q (%q) should > %d  && < %d ", k, value, begin, end))
+		return
+	}
 }
