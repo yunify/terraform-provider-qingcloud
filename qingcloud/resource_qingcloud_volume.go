b5ee04ec6c73f3ac75e8fc74e848504ff6c61362
runzexia <root@junwuhui.cn>
2017-11-17 11:07:14 +0800

    add volume description , fix volume name&description nil error , simplify code

--- qingcloud/resource_qingcloud_volume.go
+++ qingcloud/resource_qingcloud_volume.go
@@ -1,8 +1,6 @@
 package qingcloud
 
 import (
-	"fmt"
-
 	"github.com/hashicorp/terraform/helper/schema"
 	qc "github.com/yunify/qingcloud-sdk-go/service"
 )
@@ -15,17 +13,19 @@ func resourceQingcloudVolume() *schema.Resource {
 		Delete: resourceQingcloudVolumeDelete,
 		Schema: map[string]*schema.Schema{
 			"size": &schema.Schema{
-				Type:     schema.TypeInt,
-				Required: true,
-				Description: "硬盘容量，目前可创建最小 10G，最大 500G 的硬盘， 在此范围内的容量值必须是 10 的倍数	",
+				Type:        schema.TypeInt,
+				Required:    true,
+				Description: "size of volume,min 10g ,multiples of ten",
 			},
 			"name": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: true,
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "name of volume",
 			},
 			"description": &schema.Schema{
-				Type:     schema.TypeString,
-				Optional: true,
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "description of volume",
 			},
 			"type": &schema.Schema{
 				Type:         schema.TypeInt,
@@ -33,23 +33,9 @@ func resourceQingcloudVolume() *schema.Resource {
 				Default:      0,
 				ForceNew:     true,
 				ValidateFunc: withinArrayInt(0, 1, 2, 3),
-				Description: `性能型是 0
-					超高性能型是 3 (只能与超高性能主机挂载，目前只支持北京2区)，
-					容量型因技术升级过程中，在各区的 type 值略有不同:
-					  北京1区，亚太1区：容量型是 1
-					  北京2区，广东1区：容量型是 2`,
-			},
-			"instance_id": &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"device": &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"status": &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
+				Description: `performance type volume 0
+					Ultra high performance type volume is 3 (only attach to ultra high performance type instance)，
+					Capacity type,The values vary from region to region , Some region are 1 and some are 2.`,
 			},
 		},
 	}
@@ -58,22 +44,23 @@ func resourceQingcloudVolume() *schema.Resource {
 func resourceQingcloudVolumeCreate(d *schema.ResourceData, meta interface{}) error {
 	clt := meta.(*QingCloudClient).volume
 	input := new(qc.CreateVolumesInput)
+	if d.Get("name").(string) != "" {
+		input.VolumeName = qc.String(d.Get("name").(string))
+	} else {
+		input.VolumeName = qc.String(" ")
+	}
 	input.Count = qc.Int(1)
 	input.Size = qc.Int(d.Get("size").(int))
-	input.VolumeName = qc.String(d.Get("name").(string))
 	input.VolumeType = qc.Int(d.Get("type").(int))
 	output, err := clt.CreateVolumes(input)
 	if err != nil {
 		return err
 	}
 	d.SetId(qc.StringValue(output.Volumes[0]))
-	if err := motifyVolumeAttributes(d, meta, true); err != nil {
-		return err
-	}
 	if _, err = VolumeTransitionStateRefresh(clt, d.Id()); err != nil {
 		return err
 	}
-	return resourceQingcloudVolumeRead(d, meta)
+	return resourceQingcloudVolumeUpdate(d, meta)
 }
 
 func resourceQingcloudVolumeRead(d *schema.ResourceData, meta interface{}) error {
@@ -84,9 +71,6 @@ func resourceQingcloudVolumeRead(d *schema.ResourceData, meta interface{}) error
 	if err != nil {
 		return err
 	}
-	if output.RetCode != nil && qc.IntValue(output.RetCode) != 0 {
-		return fmt.Errorf("Error create tag: %s", *output.Message)
-	}
 	if len(output.VolumeSet) == 0 {
 		d.SetId("")
 		return nil
@@ -96,19 +80,11 @@ func resourceQingcloudVolumeRead(d *schema.ResourceData, meta interface{}) error
 	d.Set("description", qc.StringValue(volume.Description))
 	d.Set("size", qc.IntValue(volume.Size))
 	d.Set("type", qc.IntValue(volume.VolumeType))
-	d.Set("status", qc.StringValue(volume.Status))
-	if volume.Instance != nil {
-		d.Set("instance_id", qc.StringValue(volume.Instance.InstanceID))
-		d.Set("device", qc.StringValue(volume.Instance.Device))
-	} else {
-		d.Set("instance_id", "")
-		d.Set("device", "")
-	}
 	return nil
 }
 
 func resourceQingcloudVolumeUpdate(d *schema.ResourceData, meta interface{}) error {
-	if err := motifyVolumeAttributes(d, meta, false); err != nil {
+	if err := motifyVolumeAttributes(d, meta); err != nil {
 		return err
 	}
 	if err := changeVolumeSize(d, meta); err != nil {
