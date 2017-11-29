9f359a5a6eae1e5b8f59518ee6815be071ac0d73
runzexia <root@junwuhui.cn>
2017-11-29 14:32:31 +0800

    update LICENSE

--- main.go
+++ main.go
@@ -4,6 +4,12 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+/**
+ * Copyright (c) 2017 yunify
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
 package main
 
