ecb5c2d41d668b0b0535b913023a60b5a131cf18
runzexia <root@junwuhui.cn>
2017-12-12 10:56:04 +0800

    update installation section

---
+++ terraform/example/init/var.tf
@@ -0,0 +1,17 @@
+variable "access_key" {
+  default = "yourID"
+}
+
+variable "secret_key" {
+  default = "yourSecret"
+}
+
+variable "zone" {
+  default = "pek3a"
+}
+
+provider "qingcloud" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  zone = "${var.zone}"
+}
