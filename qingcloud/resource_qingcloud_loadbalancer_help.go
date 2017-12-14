ec39d50f562ed9786f628a14806a21fad0f4930a
runzexia <root@junwuhui.cn>
2017-12-14 16:02:03 +0800

    resize loadbalancer

--- qingcloud/resource_qingcloud_loadbalancer_help.go
+++ qingcloud/resource_qingcloud_loadbalancer_help.go
@@ -27,3 +27,26 @@ func updateLoadBalancer(lbID *string, meta interface{}) error {
 	}
 	return nil
 }
+
+func resizeLoadBalancer(lbID *string, lbType *int, meta interface{}) error {
+	clt := meta.(*QingCloudClient).loadbalancer
+	input := new(qc.ResizeLoadBalancersInput)
+	input.LoadBalancers = []*string{lbID}
+	input.LoadBalancerType = lbType
+	var output *qc.ResizeLoadBalancersOutput
+	var err error
+	simpleRetry(func() error {
+		output, err = clt.ResizeLoadBalancers(input)
+		return isServerBusy(err)
+	})
+	if err != nil {
+		return err
+	}
+	client.WaitJob(meta.(*QingCloudClient).job,
+		qc.StringValue(output.JobID),
+		time.Duration(10)*time.Second, time.Duration(1)*time.Second)
+	if _, err := LoadBalancerTransitionStateRefresh(clt, lbID); err != nil {
+		return err
+	}
+	return nil
+}
