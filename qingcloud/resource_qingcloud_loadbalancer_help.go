32d7f08d6121ab08b11698b089bd37c3ac6cb636
runzexia <root@junwuhui.cn>
2017-12-14 16:45:19 +0800

    add modify lb attributes

--- qingcloud/resource_qingcloud_loadbalancer_help.go
+++ qingcloud/resource_qingcloud_loadbalancer_help.go
@@ -1,9 +1,12 @@
 package qingcloud
 
 import (
+	"fmt"
+	"time"
+
+	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/yunify/qingcloud-sdk-go/client"
 	qc "github.com/yunify/qingcloud-sdk-go/service"
-	"time"
 )
 
 func updateLoadBalancer(lbID *string, meta interface{}) error {
@@ -93,6 +96,43 @@ func dissociateEipsToLoadBalancer(lbID *string, eips []*string, meta interface{}
 		time.Duration(10)*time.Second, time.Duration(1)*time.Second)
 	if _, err := LoadBalancerTransitionStateRefresh(clt, lbID); err != nil {
 		return err
+	}
+	return nil
+}
+
+func modifyLoadBalancerAttributes(d *schema.ResourceData, meta interface{}) error {
+	clt := meta.(*QingCloudClient).loadbalancer
+	input := new(qc.ModifyLoadBalancerAttributesInput)
+	input.LoadBalancer = qc.String(d.Id())
+	nameUpdate := false
+	descriptionUpdate := false
+	sgUpdate := false
+	ncUpdate := false
+	privateIPUpdate := false
+	input.LoadBalancerName, nameUpdate = getNamePointer(d)
+	input.Description, descriptionUpdate = getDescriptionPointer(d)
+	input.SecurityGroup, sgUpdate = getUpdateStringPointerInfo(d, resourceLoadBalancerSecurityGroupID)
+	input.NodeCount, ncUpdate = getUpdateIntPointerInfo(d, resourceLoadBalancerNodeCount)
+	if d.HasChange(resourceLoadBalancerPrivateIPs) {
+		privateIPs := d.Get(resourceLoadBalancerPrivateIPs).(*schema.Set).List()
+		if len(privateIPs) != 1 || d.Get(resourceLoadBalancerVxnetID).(string) == "vxnet-0" {
+			return fmt.Errorf("error private_ips info")
+		}
+		input.PrivateIP = qc.String(privateIPs[0].(string))
+		privateIPUpdate = true
+	}
+	if nameUpdate || descriptionUpdate || sgUpdate || ncUpdate || privateIPUpdate {
+		var err error
+		simpleRetry(func() error {
+			_, err = clt.ModifyLoadBalancerAttributes(input)
+			return isServerBusy(err)
+		})
+		if err != nil {
+			return err
+		}
+	}
+	if sgUpdate || ncUpdate || privateIPUpdate {
+		updateLoadBalancer(qc.String(d.Id()), meta)
 	}
 	return nil
 }
