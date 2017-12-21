7c1bd326b65cbfd928df47a58b786f82604a364d
runzexia <root@junwuhui.cn>
2017-12-21 14:19:55 +0800

    fix windows encoding problem

--- terraform/example/wordpress/modules/wordpress/install_docker.sh
+++ terraform/example/wordpress/modules/wordpress/install_docker.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/bin/bash 
 curl -fsSL get.docker.com -o get-docker.sh 
-sh get-docker.sh --mirror Aliyun
+sh get-docker.sh --mirror Aliyun 
+mkdir /etc/docker
 cat >> /etc/docker/daemon.json <<EOF
-      {
-        "registry-mirrors": ["https://registry.docker-cn.com"]
-      }
+{
+  "registry-mirrors": ["https://registry.docker-cn.com"] 
+}
 EOF
-systemctl start docker
+systemctl start docker 
