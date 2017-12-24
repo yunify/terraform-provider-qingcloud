c64f4c6416501cbbbae2767f1005916e39c22a75
runzexia <root@junwuhui.cn>
2017-12-24 15:44:29 +0800

    add resource lb listener

--- qingcloud/resource_qingcloud_loadbalancer_listener.go
+++ qingcloud/resource_qingcloud_loadbalancer_listener.go
@@ -1,6 +1,9 @@
 package qingcloud
 
-import "github.com/hashicorp/terraform/helper/schema"
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+	qc "github.com/yunify/qingcloud-sdk-go/service"
+)
 
 const (
 	resourceLoadBalancerListenerLBId                = "load_balancer_id"
@@ -17,10 +20,10 @@ const (
 
 func resourceQingcloudLoadBalancerListener() *schema.Resource {
 	return &schema.Resource{
-		Create: ,
-		Read:   ,
-		Update: ,
-		Delete: ,
+		Create: resourceQingcloudLoadBalancerListnerCreate,
+		Read:   resourceQingcloudLoadBalancerListenerRead,
+		Update: resourceQingcloudLoadBalancerListenerUpdate,
+		Delete: resourceQingcloudLoadBalancerListnerDestroy,
 		Schema: map[string]*schema.Schema{
 			resourceName: &schema.Schema{
 				Type:     schema.TypeString,
@@ -59,9 +62,10 @@ func resourceQingcloudLoadBalancerListener() *schema.Resource {
 				Computed: true,
 			},
 			resourceLoadBalancerListenerForwardfor: &schema.Schema{
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
+				Type:         schema.TypeInt,
+				Optional:     true,
+				Computed:     true,
+				ValidateFunc: withinArrayIntRange(0, 7),
 			},
 			resourceLoadBalancerListenerHealthCheckMethod: &schema.Schema{
 				Type:     schema.TypeString,
@@ -74,10 +78,122 @@ func resourceQingcloudLoadBalancerListener() *schema.Resource {
 				Computed: true,
 			},
 			resourceLoadBalancerListenerOption: &schema.Schema{
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
+				Type:         schema.TypeInt,
+				Optional:     true,
+				Computed:     true,
+				ValidateFunc: withinArrayIntRange(0, 15),
 			},
 		},
 	}
+}
+
+func resourceQingcloudLoadBalancerListnerCreate(d *schema.ResourceData, meta interface{}) error {
+	clt := meta.(*QingCloudClient).loadbalancer
+	input := new(qc.AddLoadBalancerListenersInput)
+	listener := new(qc.LoadBalancerListener)
+	input.LoadBalancer = getSetStringPointer(d, resourceLoadBalancerListenerLBId)
+	listener.LoadBalancerListenerName = getSetStringPointer(d, resourceName)
+	listener.ListenerPort = qc.Int(d.Get(resourceLoadBalancerListenerPort).(int))
+	listener.ListenerProtocol = getSetStringPointer(d, resourceLoadBalancerListenerProtocol)
+	listener.BackendProtocol = getSetStringPointer(d, resourceLoadBalancerListenerProtocol)
+	listener.BalanceMode = getSetStringPointer(d, resourceLoadBalancerListenerBalancerMode)
+	listener.ServerCertificateID = getSetStringPointer(d, resourceLoadBalancerListenerServerCertificateId)
+	listener.SessionSticky = getSetStringPointer(d, resourceLoadBalancerListenerSessionSticky)
+	listener.Forwardfor = qc.Int(d.Get(resourceLoadBalancerListenerForwardfor).(int))
+	listener.HealthyCheckMethod = getSetStringPointer(d, resourceLoadBalancerListenerHealthCheckMethod)
+	listener.HealthyCheckOption = getSetStringPointer(d, resourceLoadBalancerListenerHealthCheckOption)
+	listener.ListenerOption = qc.Int(d.Get(resourceLoadBalancerListenerOption).(int))
+
+	input.Listeners = []*qc.LoadBalancerListener{listener}
+	var output *qc.AddLoadBalancerListenersOutput
+	var err error
+	simpleRetry(func() error {
+		output, err = clt.AddLoadBalancerListeners(input)
+		return isServerBusy(err)
+	})
+	if err != nil {
+		return err
+	}
+	if err := updateLoadBalancer(qc.String(d.Get(resourceLoadBalancerListenerLBId).(string)), meta); err != nil {
+		return nil
+	}
+	d.SetId(qc.StringValue(output.LoadBalancerListeners[0]))
+	return resourceQingcloudLoadBalancerListenerRead(d, meta)
+}
+
+func resourceQingcloudLoadBalancerListenerRead(d *schema.ResourceData, meta interface{}) error {
+	clt := meta.(*QingCloudClient).loadbalancer
+	input := new(qc.DescribeLoadBalancerListenersInput)
+	input.LoadBalancerListeners = []*string{qc.String(d.Id())}
+	input.LoadBalancer = getSetStringPointer(d, resourceLoadBalancerListenerLBId)
+	var output *qc.DescribeLoadBalancerListenersOutput
+	var err error
+	simpleRetry(func() error {
+		output, err = clt.DescribeLoadBalancerListeners(input)
+		return isServerBusy(err)
+	})
+	if err != nil {
+		return err
+	}
+	if len(output.LoadBalancerListenerSet) == 0 {
+		d.SetId("")
+		return nil
+	}
+	d.Set(resourceName, qc.StringValue(output.LoadBalancerListenerSet[0].LoadBalancerListenerName))
+	d.Set(resourceLoadBalancerListenerPort, qc.IntValue(output.LoadBalancerListenerSet[0].ListenerPort))
+	d.Set(resourceLoadBalancerListenerProtocol, qc.StringValue(output.LoadBalancerListenerSet[0].ListenerProtocol))
+	d.Set(resourceLoadBalancerListenerBalancerMode, qc.StringValue(output.LoadBalancerListenerSet[0].BalanceMode))
+	d.Set(resourceLoadBalancerListenerServerCertificateId, qc.StringValue(output.LoadBalancerListenerSet[0].ServerCertificateID))
+	d.Set(resourceLoadBalancerListenerSessionSticky, qc.StringValue(output.LoadBalancerListenerSet[0].SessionSticky))
+	d.Set(resourceLoadBalancerListenerForwardfor, qc.IntValue(output.LoadBalancerListenerSet[0].Forwardfor))
+	d.Set(resourceLoadBalancerListenerHealthCheckMethod, qc.StringValue(output.LoadBalancerListenerSet[0].HealthyCheckMethod))
+	d.Set(resourceLoadBalancerListenerHealthCheckOption, qc.StringValue(output.LoadBalancerListenerSet[0].HealthyCheckOption))
+	d.Set(resourceLoadBalancerListenerOption, qc.IntValue(output.LoadBalancerListenerSet[0].ListenerOption))
+	return nil
+}
+
+func resourceQingcloudLoadBalancerListenerUpdate(d *schema.ResourceData, meta interface{}) error {
+	clt := meta.(*QingCloudClient).loadbalancer
+	input := new(qc.ModifyLoadBalancerListenerAttributesInput)
+	input.LoadBalancerListenerName = getSetStringPointer(d, resourceName)
+	input.BalanceMode = getSetStringPointer(d, resourceLoadBalancerListenerBalancerMode)
+	input.ServerCertificateID = getSetStringPointer(d, resourceLoadBalancerListenerServerCertificateId)
+	input.SessionSticky = getSetStringPointer(d, resourceLoadBalancerListenerSessionSticky)
+	input.Forwardfor = qc.Int(d.Get(resourceLoadBalancerListenerForwardfor).(int))
+	input.HealthyCheckMethod = getSetStringPointer(d, resourceLoadBalancerListenerHealthCheckMethod)
+	input.HealthyCheckOption = getSetStringPointer(d, resourceLoadBalancerListenerHealthCheckOption)
+	//TODO input.ListenerOption =  qc.Int(d.Get(resourceLoadBalancerListenerOption).(int))
+	var output *qc.ModifyLoadBalancerListenerAttributesOutput
+	var err error
+	simpleRetry(func() error {
+		output, err = clt.ModifyLoadBalancerListenerAttributes(input)
+		return isServerBusy(err)
+	})
+	if err != nil {
+		return err
+	}
+	if err := updateLoadBalancer(qc.String(d.Get(resourceLoadBalancerListenerLBId).(string)), meta); err != nil {
+		return nil
+	}
+	return resourceQingcloudVpcStaticRead(d, meta)
+}
+
+func resourceQingcloudLoadBalancerListnerDestroy(d *schema.ResourceData, meta interface{}) error {
+	clt := meta.(*QingCloudClient).loadbalancer
+	input := new(qc.DeleteLoadBalancerListenersInput)
+	input.LoadBalancerListeners = []*string{qc.String(d.Id())}
+	var output *qc.DeleteLoadBalancerListenersOutput
+	var err error
+	simpleRetry(func() error {
+		output, err = clt.DeleteLoadBalancerListeners(input)
+		return isServerBusy(err)
+	})
+	if err != nil {
+		return err
+	}
+	if err := updateLoadBalancer(qc.String(d.Get(resourceLoadBalancerListenerLBId).(string)), meta); err != nil {
+		return nil
+	}
+	d.SetId("")
+	return nil
 }
