531d47caa243deba68b469e8132c595bc803b843
runzexia <root@junwuhui.cn>
2018-01-24 12:35:00 +0800

    Make the timeout period longer.

--- qingcloud/constants.go
+++ qingcloud/constants.go
@@ -25,7 +25,7 @@ const (
 
 	DEFAULT_ZONE           = "pek3a"
 	DEFAULT_ENDPOINT       = "https://api.qingcloud.com:443/iaas"
-	waitJobTimeOutDefault  = 120
+	waitJobTimeOutDefault  = 240
 	waitJobIntervalDefault = 5
 
 	resourceName        = "name"
