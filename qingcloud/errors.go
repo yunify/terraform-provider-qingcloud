562f276e32c27d9c7270b85ffa1e852ac909093e
runzexia <root@junwuhui.cn>
2018-11-06 16:18:15 +0800

    change license to MPL2.0

--- qingcloud/errors.go
+++ qingcloud/errors.go
@@ -1,16 +1,3 @@
-/**
- * Copyright (c) 2016 Magicshui
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-/**
- * Copyright (c) 2017 yunify
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 package qingcloud
 
 const SERVERBUSY = 5100
